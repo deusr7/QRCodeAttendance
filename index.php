4ff3c5c03d5b93e160f86fd2b112d77372e94984
Kaizenn <divinerapure@gmail.com>
2023-07-01 02:07:45 +0800

    second commit

--- index.php
+++ index.php
@@ -26,6 +26,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
 
+    <link rel="icon" href="images/sharingan.png" type="image/x-icon">
+
 
     <link rel="stylesheet" href="css/styles1.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
